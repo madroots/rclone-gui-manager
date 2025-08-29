d839a646367fb49d89714f2c19709ef9aceaa877
madroots <simon.barker@linuxmail.org>
2025-08-29 23:54:34 +0200

    Fix AppImage structure to avoid path duplication issues

--- build-appimage.sh
+++ build-appimage.sh
@@ -16,15 +16,15 @@ docker run --rm -v "$(pwd)/..":/app -w /app ubuntu:20.04 bash -c "
   chmod +x python.AppImage &&
   ./python.AppImage --appimage-extract >/dev/null 2>&1 &&
   mkdir -p AppDir/usr/bin &&
-  mkdir -p AppDir/usr/share/applications &&
-  mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps &&
-  mv squashfs-root/* AppDir/ &&
+  cp -r squashfs-root/usr/bin/* AppDir/usr/bin/ &&
+  cp -r squashfs-root/usr/lib AppDir/usr/ &&
+  cp -r squashfs-root/usr/share AppDir/usr/ || echo \"No share directory to copy\" &&
+  cp -r squashfs-root/opt AppDir/ || echo \"No opt directory to copy\" &&
   cp /app/rclone-gui-manager.py AppDir/usr/bin/ &&
   chmod 755 AppDir/usr/bin/rclone-gui-manager.py &&
   echo '#!/bin/bash' > AppDir/usr/bin/rclone-gui-manager &&
-  echo 'HERE=\"\$(dirname \"\$0\")\"' >> AppDir/usr/bin/rclone-gui-manager &&
-  echo 'export PYTHONPATH=\"\$HERE:\$PYTHONPATH\"' >> AppDir/usr/bin/rclone-gui-manager &&
-  echo '\"\$HERE/python3.9\" \"\$HERE/rclone-gui-manager.py\" \"\$@\"' >> AppDir/usr/bin/rclone-gui-manager &&
+  echo 'DIR=\"$(dirname \"$0\")\"' >> AppDir/usr/bin/rclone-gui-manager &&
+  echo '\"$DIR/python3.9\" \"$DIR/rclone-gui-manager.py\" \"$@\"' >> AppDir/usr/bin/rclone-gui-manager &&
   chmod 755 AppDir/usr/bin/rclone-gui-manager &&
   echo '[Desktop Entry]' > AppDir/rclone-gui-manager.desktop &&
   echo 'Type=Application' >> AppDir/rclone-gui-manager.desktop &&
@@ -43,8 +43,8 @@ docker run --rm -v "$(pwd)/..":/app -w /app ubuntu:20.04 bash -c "
     chmod 644 AppDir/usr/share/icons/hicolor/256x256/apps/rclone-gui-manager.svg
   fi &&
   echo '#!/bin/bash' > AppDir/AppRun &&
-  echo 'HERE=\"\$(dirname \"\$(readlink -f \"\$0\")\")\"' >> AppDir/AppRun &&
-  echo 'exec \"\$HERE/usr/bin/rclone-gui-manager\" \"\$@\"' >> AppDir/AppRun &&
+  echo 'HERE=\"$(dirname \"$(readlink -f \"$0\")\")\"' >> AppDir/AppRun &&
+  echo 'exec \"$HERE/usr/bin/rclone-gui-manager\" \"$@\"' >> AppDir/AppRun &&
   chmod 755 AppDir/AppRun &&
   wget -O appimagetool \"https://github.com/AppImage/appimagetool/releases/download/1.9.0/appimagetool-x86_64.AppImage\" &&
   chmod +x appimagetool &&
